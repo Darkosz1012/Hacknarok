2e515e5ae32f7d2e2ecf495c0bcddd0276c4f69f
Jakub <quban123@gmail.com>
2019-04-07 01:19:01 +0200

    homePage #2

--- vue/src/views/homePage.vue
+++ vue/src/views/homePage.vue
@@ -1,86 +1,60 @@
 <template>
-    <main class="aplication">
-        <nav> </nav>
-        <ul id="slide-out" class="sidenav">
-            <li class="active"><a href="#" class="waves-effect">
-                    <i class="material-icons">dashboard</i>
-                    <span class="nav-text">Panel Główny</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">people</i>
-                    <span class="nav-text">Użytkownicy</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">folder</i>
-                    <span class="nav-text">Menedżer plików</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">recent_actors</i>
-                    <span class="nav-text">Osoby</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">settings_applications</i>
-                    <span class="nav-text">Główna konfiguracja</span>
-            </a></li>
-            <li><div class="divider"></div></li>
-            <li>
-                <a class="subheader">Podstony</a>
-            </li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">home</i>
-                    <span class="nav-text">Strona główna</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">pages</i>
-                    <span class="nav-text">Aktualności</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">event_note</i>
-                    <span class="nav-text">Wydarzenia</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">school</i>
-                    <span class="nav-text">Specjalizacje</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">card_membership</i>
-                    <span class="nav-text">O Szkole</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">photo_library</i>
-                    <span class="nav-text">Galeria</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">library_books</i>
-                    <span class="nav-text">Dokumenty</span>
-            </a></li>
-            <li><a href="#" class="waves-effect">
-                    <i class="material-icons">contacts</i>
-                    <span class="nav-text">Kontakt</span>
-            </a></li>
-        </ul>
-        <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-    </main>
+  <main class="aplication">
+    <ul id="slide-out" class="sidenav">
+      <li>
+        <div class="user-view">
+          <div class="background">
+            <img src="@/assets/background.jpg">
+          </div>
+          <a href="#user">
+            <img class="circle" src="@/assets/background.jpg">
+          </a>
+          <a href="#name">
+            <span class="white-text name">John Doe</span>
+          </a>
+          <a href="#email">
+            <span class="white-text email">jdandturk@gmail.com</span>
+          </a>
+        </div>
+      </li>
+      <li>
+        <a class="subheader">Grupy</a>
+      </li>
+      <li class="active">
+        <a href="#" class="waves-effect">
+            <span class="nav-text">Koło Naukowe</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="waves-effect">
+          <span class="nav-text">Kumple</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="waves-effect">
+          <span class="nav-text">Grupa Biznesowa</span>
+        </a>
+      </li>
+    </ul>
+    <a href="#" data-target="slide-out" class="sidenav-trigger aplication__sideNavButton">
+      <i class="material-icons">menu</i>
+    </a>
+  </main>
 </template>
 
 <style lang="scss">
-    @import "@/sass/homePage.scss";
+@import "@/sass/homePage.scss";
 </style>
 
 <script>
-  export default {
-  name: 'homePage',
+export default {
+  name: "homePage",
   data() {
-    return {
-      
-    }
+    return {};
   },
-  methods: {
-          
-  },
+  methods: {},
   mounted() {
-          
-   M.AutoInit();
+    M.AutoInit();
   }
-}
+};
 </script>
