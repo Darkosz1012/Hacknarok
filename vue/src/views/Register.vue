50a23405aa2ccd62621c70c536d0d0d38c1e8508
Jakub <quban123@gmail.com>
2019-04-06 21:53:11 +0200

    Vue #1 componenty

---
+++ vue/src/views/Register.vue
@@ -0,0 +1,30 @@
+<template>
+    <main class="loginPanel z-depth-2">
+        <div class="loginPanel__image">
+            <img src="@/assets/background.jpg" alt="tlo"/>
+        </div>
+        <LoginForm :isLogin="false" />
+    </main>
+</template>
+
+<style lang="scss">
+    @import "@/sass/login.scss";
+</style>
+
+<script>
+import Form from './Form'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      user: {
+        name: '',
+        pass: ''
+      }
+    }
+  },
+  components: {
+    Form
+  }
+}
+</script>
