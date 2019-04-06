11ba19de58eef0a42234bcd8b8f1310a00db05ff
ostatni5 <ostatni5@o2.pl>
2019-04-06 17:10:28 +0200

    vue2

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,41 @@
 <template>
+
+
   <div id="app">
+  <l-map id="myMap" ref="myMap">
+  <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+  </l-map>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    
   </div>
 </template>
 
+<script>
+  export default {
+    mounted () {
+      navigator.geolocation.getCurrentPosition(function (location) {
+
+        var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+        mymap.setView(latlng);
+
+        var marker = L.marker(latlng).addTo(mymap);
+      })
+      // this.$nextTick(() => {
+      //   this.$refs.myMap.mapObject.ANY_LEAFLET_MAP_METHOD();
+      // })
+    }
+  }
+</script>
+
 <style lang="scss">
+#myMap{
+  height:150px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
